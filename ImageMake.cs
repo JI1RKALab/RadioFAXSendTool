31f0c1eb74e4748ce9390adc22185eb3baac913b
JI1RKA <ji1rkafax@gmail.com>
2023-04-29 18:57:36 +0900

    パッケージバージョンアップ・画像生成クラス(WIP)

---
+++ ImageMake.cs
@@ -0,0 +1,42 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace net.sictransit.wefax
+{
+    public class ImageMake
+    {
+        public string MakeImage(string imageFilename) 
+        {
+
+            using (SixLabors.ImageSharp.Image ImageData = SixLabors.ImageSharp.Image.Load<Rgb24>(imageFilename))
+            {
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+            return "";
+        }
+    }
+}
